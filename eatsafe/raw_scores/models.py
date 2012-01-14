f117f7b496f809fa8bfb6936421232c10e1bb7e0
Aurelio Tinio <aurelio@tinio.org>
2012-01-14 15:33:25 -0800

    add RawScores model as spat out by inspectdb

--- eatsafe/raw_scores/models.py
+++ eatsafe/raw_scores/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class RawScores(models.Model):
+    name = models.CharField(max_length=-1)
+    zip = models.TextField() # This field type is a guess.
+    inspect_date = models.CharField(max_length=-1)
+    score = models.SmallIntegerField()
+    address = models.CharField(max_length=-1)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    class Meta:
+        db_table = u'raw_scores'
