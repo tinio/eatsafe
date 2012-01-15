4c34f040f247e2e2587d200ece0b513c184cf1f1
Aurelio Tinio <aurelio@tinio.org>
2012-01-14 16:46:16 -0800

    create table with primary id

--- sql/create-table-raw-scores.sql
+++ sql/create-table-raw-scores.sql
@@ -1,1 +1,1 @@
-create table raw_scores (NAME varchar, ZIP char(5), INSPECT_DATE varchar, SCORE smallint, ADDRESS varchar, LATITUDE double precision, LONGITUDE double precision);
+create table raw_scores (ID integer, NAME varchar, ZIP char(5), INSPECT_DATE varchar, SCORE smallint, ADDRESS varchar, LATITUDE double precision, LONGITUDE double precision);
