0a61d6db646291429d9e4c209641c41ee58fc6a1
Aurelio Tinio <aurelio@tinio.org>
2012-01-14 18:31:38 -0800

    tie front and back end

--- eatsafe/urls.py
+++ eatsafe/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
+
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'eatsafe.views.home', name='home'),
     # url(r'^eatsafe/', include('eatsafe.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
@@ -14,4 +15,13 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('django.views.generic.simple',
+    (r'^style.css$', 'direct_to_template', {'template' : 'style.css'}),
+    (r'^fakejson$',  'direct_to_template', {'template' : 'fakejson'}),
+)
+
+urlpatterns += patterns('',
+    url(r'^$', 'home.views.index', name='home_index'),
 )
